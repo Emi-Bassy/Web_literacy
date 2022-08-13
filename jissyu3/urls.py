3a4306863f6d2791eaa14bdbc3dffe926aa8df47
EMI ICHIMURA <s1F102000488@iniad.org>
2022-08-13 14:52:57 +0900

    ログイン機能の実装
    ログイン画面、管理者画面の作成

--- jissyu3/urls.py
+++ jissyu3/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("room/<str:room_name>", views.room, name="room")
+    path("room/<str:room_name>", views.room, name="room"),
+    path("roomadmin", views.roomadmin, name="roomadmin"),
+    path("login", views.login, name="login")
 ]
--- jissyu3/views.py
+++ jissyu3/views.py
@@ -1,5 +1,5 @@
 import re
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
@@ -7,3 +7,19 @@ def index(request):
 
 def room(request, room_name):
     return render(request, "room.html", {"room_name": room_name})
+
+def roomadmin(request):
+    if not "logined" in request.session:
+        return redirect("login")
+    return render(request, "roomadmin.html")
+
+def login(request):
+    if request.method == "POST":
+        if request.POST["password"] == "password":
+            request.session["logined"] = True
+            return redirect("roomadmin")
+    else:
+        if not "logined" in request.session:
+            return render(request, "login.html")
+        else:
+            return redirect("roomadmin")                      
