2bdfea215463fb73e228d52500d0490476acb404
akahoshi1421 <akaho1421@gmail.com>
2022-10-29 23:19:25 +0900

    cicd環境(改正)と進捗度のcssが見えなくなる問題を修正

--- cicd.sh
+++ cicd.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
 sudo systemctl stop nginx
+sudo systemctl stop supervisor
 git pull origin main
-python3 manage.py collectstatic
-
-if [ $# = 1 ]; then
-    if [ "$1" = "db" ]; then
-        python3 manage.py makemigrations
-        python3 manage.py migrate
-    fi
-fi
-
-sudo supervisorctl reread
-sudo supervisorctl update
+sudo systemctl start supervisor
 sudo systemctl start nginx
